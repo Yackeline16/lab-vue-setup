db3ed39604cff1d011d3d4134060dcb7dcaa96b4
Yackeline16 <jacqueline@capa8.net>
2022-08-02 00:26:35 +0200

    First commit

---
+++ lab/src/components/ComponentPrueba.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{texto}}</h1>
+    <a @click="myFuction" :href="link" target="_blank">Este es mi enlance</a>
+    <input type="text" v-model="myInput" />
+    <a href="#">Tu enlace {{myInput}}</a>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentPrueba',
+  props: {
+    texto: String
+  },
+data(){
+  return {
+    link:"http://microsoft.com",
+    myInput: "",
+  };
+}, 
+methods: {
+  myFuction(){
+    alert('Has hecho click XD');
+  },
+},
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ lab/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <ComponentPrueba texto="pepito"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue';
+import ComponentPrueba from '@/components/ComponentPrueba.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    // HelloWorld,
+    ComponentPrueba
+  }
+}
+</script>
